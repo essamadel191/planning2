a886b159958c068e9b2b3bcb4c3ecc606260ed62
essamLaptop <essam.adel191@gmail.com>
2024-11-18 12:36:55 +0200

    Update Product Controller

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,35 +1,27 @@
 using Core.Entities;
-using Infrastructure.Data;
-using Microsoft.AspNetCore.Http;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IProductRepository repo) : ControllerBase
     {
-        private readonly StoreContext context;
-
-        public ProductsController(StoreContext context)
-        {
-            this.context = context;
-        }
 
         // ActionResults Allow Us to return HTTP responses
         // IEnumerable Type of List of Type Prodcut
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Products>>> GetProducts()
         {
-            return await context.Products.ToListAsync();
+            return Ok(await repo.GetProductAsync());
         }
 
         // For Specific Product
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Products>> GetProduct(int id)
         {
-            var Product = await context.Products.FindAsync(id);
+            var Product = await repo.GetProductByIdAsync(id);
 
             // Definsive Check for the product
             if(Product == null) return NotFound();
@@ -40,47 +32,60 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<Products>> CreateProduct(Products product)
         {
-            context.Products.Add(product);
 
-            await context.SaveChangesAsync();
-            
-            return product;
+            repo.Addproduct(product);
+
+            if(await repo.SaveChangesAsync()) 
+            {
+                return CreatedAtAction("GetProduct", new {id = product.Id},product); 
+            }
+
+            return BadRequest("Problem creating product");
         }
         
         [HttpPut("{id:int}")]
         public async Task<ActionResult> UpdateProduct(int id,Products product)
         {
+            
             if (product.Id != id || !ProductExists(id)) 
                 return BadRequest("Connot Update this product");
 
             // This tell the entity framework tracker that we're passing in here is 
             // an entity effectivily abd has been modified
-            context.Entry(product).State = EntityState.Modified;
-            
-            await context.SaveChangesAsync();
+            repo.UpdateProduct(product);
 
-            return NoContent();
+            if(await repo.SaveChangesAsync())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Problem updating the product");
         }
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
-            var product = await context.Products.FindAsync(id);
+            var product = await repo.GetProductByIdAsync(id);
 
             if (product == null) return NotFound("This Product of id : "+id+" is not found");
 
             // This mean that dotnet is tracking this product
-            context.Products.Remove(product);
+            repo.DeleteProduct(product);
 
             // Then Update the database 
-            await context.SaveChangesAsync();
+            await repo.SaveChangesAsync();
 
-            return NoContent();
+            if(await repo.SaveChangesAsync())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Problem deleting the product");
         }
 
         private bool ProductExists(int id)
         {
-            return context.Products.Any(x => x.Id == id);
+            return repo.ProductExists(id);
         }
     }
 }
